ae6038ab6058e1c79bee67c596d8f6fb9ce5a0bf
arumbaua366 <arumbaua366@gmail.com>
2023-01-25 23:57:48 -0800

    create ChatEngine admin details

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from "react-chat-engine";
 
 import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="22511e7e-57eb-418b-ae36-77ed82c12d87"
+      userName="ar366"
+      userSecret="123123"
+    />
+  );
+};
+
+export default App;
