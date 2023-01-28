f1ebd7a2d113fdbd3a2a84f7584401eb0534a103
arumbaua366 <arumbaua366@gmail.com>
2023-01-28 00:14:17 -0800

    complete TheirMessage component (similar code to MyMessage component)

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,5 +1,39 @@
-const TheirMessage = () => {
-  return <div>TheirMessage</div>;
+const TheirMessage = ({ lastMessage, message }) => {
+  //receiving last message and message itself
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+  return (
+    // div is generated only if there is a first message by the user
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments?.length > 0 ? (
+        //if this message is an actual message or an image. if message is 0, then it is an image
+        <img
+          src={message.attachment[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#cabcdc",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TheirMessage;
